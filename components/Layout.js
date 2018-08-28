22ff758eb61c63fdde0d6b236c0565c1debbb5a1
krokowski0x <krokowski.dx@gmail.com>
2018-08-28 17:51:05 +0200

    Added separate Welcome component

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
+import Head from "next/head";
+import GitHubLink from "../components/GitHubLink";
 
 export default props => {
   return (
     <Container>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"
+        />
+      </Head>
       {props.children}
+      <footer />
       <style jsx global>{`
         body {
-          margin: 5%;
           font-family: "Roboto", sans-serif;
           color: #fff;
           background-color: #181717;
