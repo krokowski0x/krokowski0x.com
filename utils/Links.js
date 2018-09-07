24e1b135db64affbeca76246fb2d0e6f3f0d2ccd
krokowski0x <krokowski.dx@gmail.com>
2018-09-08 00:04:44 +0200

    Added custom icons for different social media links

---
+++ utils/Links.js
@@ -0,0 +1,13 @@
+const Links = {
+  GitHub: "https://github.com/krokowski0x",
+  CodeTrace: "https://github.com/krokowski0x",
+  Facebook: "https://github.com/krokowski0x",
+  Instagram: "https://github.com/krokowski0x",
+  LinkedIn: "https://github.com/krokowski0x",
+  Twitter: "https://github.com/krokowski0x",
+  Gmail: "https://github.com/krokowski0x",
+  Medium: "https://github.com/krokowski0x",
+  Resume: "https://github.com/krokowski0x"
+};
+
+export default Links;
