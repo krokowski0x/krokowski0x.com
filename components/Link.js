7c85650a51b2d943ea4943a46bcc43b35276ff51
krokowski0x <krokowski.dx@gmail.com>
2018-09-09 20:43:37 +0200

    Refactored Icons' svg's into reusable components

--- components/Link.js
+++ components/Link.js
@@ -1,10 +1,19 @@
 import Icons from "simple-icons";
 import Links from "../utils/Links";
+import { GitHub } from "../utils/Icons";
 
 const Link = props => {
   return (
     <a target="_blank" rel="noopener noreferrer" href={Links[props.icon]}>
-      {Icons["GitHub"].svg}
+      <svg
+        aria-labelledby="simpleicons-github-icon"
+        role="img"
+        fill="white"
+        height="50px"
+        viewBox="0 0 24 24"
+      >
+        <GitHub />
+      </svg>
     </a>
   );
 };
